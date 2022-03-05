5ed7fa7d04985e1845d4de8ce53def5364863249
Vikash Kumar <86666736+vikashkumar2701@users.noreply.github.com>
2022-03-05 12:12:34 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="title">TechFest by SRM</div>
+
+        <div className="navcontainer">
+          <div className="nav">
+            <div className="menu">Home</div>
+            <div className="menu">Events</div>
+            <div className="menu">Registration</div>
+            <div className="menu">Contact Us</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
